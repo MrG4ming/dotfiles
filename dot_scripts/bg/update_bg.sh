cb9546872fbd55dbda90d71a5a51db5e3bb218c5
MrG4ming <wasmuthgero@gmail.com>
2024-03-08 15:17:22 +0100

    adding another bg

--- dot_scripts/bg/update_bg.sh
+++ dot_scripts/bg/update_bg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/admin/.scripts/bg/
-sudo sh /home/admin/.scripts/bg/set_bg.sh daynight /home/admin/Pictures/Background/Morning/pepe-mountains-morning.png /home/admin/Pictures/Background/Sunset/pepe-wp.jpg /home/admin/Pictures/Background/Sunset/pepe-wp.jpg /home/admin/Pictures/Background/Night/pepe-night-mountains.png
+sh /home/admin/.scripts/bg/set_bg.sh daynight /home/admin/Pictures/Background/Morning/pepe-mountains-morning.png /home/admin/Pictures/Background/Midday/pepe-mountains-landscape-clouds-sky.png /home/admin/Pictures/Background/Sunset/pepe-wp.jpg /home/admin/Pictures/Background/Night/pepe-night-mountains.png
